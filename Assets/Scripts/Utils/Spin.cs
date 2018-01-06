0b88d3803a68cf73c9eb68a4f3bfc02a46ddc5c9
pautenavidal <pautenavidal@gmail.com>
2018-01-06 21:13:57 +0100

    Added FireExplosionEffects

---
+++ Assets/Scripts/Utils/Spin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+	public float speed =50f;
+	public Vector3 direction = Vector3.left;
+
+
+	void Update ()
+	{
+		transform.Rotate(direction, speed * Time.deltaTime);
+	}
+}
