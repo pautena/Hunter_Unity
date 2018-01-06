0bfc6b219c03de18192db9a9b5044da41b8e5063
pautenavidal <pautenavidal@gmail.com>
2018-01-06 18:53:58 +0100

    Fade animations done

---
+++ Assets/Scripts/UI/ImageFade.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageFade: MonoBehaviour {
+
+	// the image you want to fade, assign in inspector
+	public Image img;
+	public float fadeDuration = 2000f;
+
+	public void Awake(){
+		StartCoroutine(FadeIn());
+	}
+
+	public IEnumerator FadeIn(){
+		return FadeImage (true);
+	}
+
+	public IEnumerator FadeOut(){
+		return FadeImage (false);
+	}
+
+	private IEnumerator FadeImage(bool fadeAway){
+		// fade from opaque to transparent
+		if (fadeAway){
+			// loop over 1 second backwards
+			for (float i = fadeDuration; i >= 0; i -= Time.deltaTime){
+				// set color with i as alpha
+				img.color = new Color(1, 1, 1, i);
+				yield return null;
+			}
+		}
+		// fade from transparent to opaque
+		else{
+			// loop over 1 second
+			for (float i = 0; i <= fadeDuration; i += Time.deltaTime){
+				// set color with i as alpha
+				img.color = new Color(1, 1, 1, i);
+				yield return null;
+			}
+		}
+	}
+}
