2277dc7cb667ba6602fd289c1981505c66e70851
pautenavidal <pautenavidal@gmail.com>
2018-01-09 21:19:56 +0100

    Added new script for camera

---
+++ Assets/Scripts/Camera/LoginMovementCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginMovementCamera : MonoBehaviour {
+
+	public float startCameraXVelocity = 10.0f;
+	public float minCameraXMovement = -550.0f;
+	public float maxCameraXMovement = 550.0f;
+
+	private float cameraXVelocity;
+	private Transform cameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		cameraXVelocity = startCameraXVelocity;
+		cameraTransform = GetComponent<Transform> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		cameraTransform.Translate (new Vector3(cameraXVelocity,0.0f,0.0f) * Time.deltaTime);
+
+		float x = cameraTransform.position.x;
+		if (x > maxCameraXMovement || x <minCameraXMovement)
+			cameraXVelocity *= -1;
+	}
+}
