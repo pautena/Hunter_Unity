f867f5543b1a6337920d2a1d73da5d370cbd246f
pautenavidal <pautenavidal@gmail.com>
2018-01-10 10:42:55 +0100

    Added manager to Poi

---
+++ Assets/Scripts/Poi/PointOfInterest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointOfInterest : MonoBehaviour {
+
+	public bool enabled=true;
+
+	public GameObject enabledGameObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//TODO: Remove this line when connected to NEM
+		if (enabled) {
+			Enable ();
+		} else {
+			Disable ();
+		}		
+	}
+
+
+	public void Enable(){
+		SetEnabled (true);
+	}
+
+	public void Disable(){
+		SetEnabled (false);
+	}
+
+	private void SetEnabled(bool enabled){
+		enabledGameObject.SetActive (enabled);
+	}
+}
